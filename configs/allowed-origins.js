2fb8b3377a8364b102571ddd30a5ad0c70c80a6e
Vulpes231 <whykayshot@gmail.com>
2023-12-27 11:09:10 +0100

    fix

--- configs/allowed-origins.js
+++ configs/allowed-origins.js
@@ -1,7 +1,7 @@
-const corsOptions = {
-  origin: [
-    "http://localhost:5173",
-    "https://247shipslimited.com",
-    "https://admin.247shipslimited.com",
-  ],
-};
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://247shipslimited.com",
+  "https://admin.247shipslimited.com",
+];
+
+module.exports = allowedOrigins;
